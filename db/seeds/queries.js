8c76e6cccc3c581df62c3afe1220de4882824f1c
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-05-30 13:49:04 +0100

    Add low-level ERD

--- db/seeds/queries.js
+++ db/seeds/queries.js
@@ -1,12 +1,21 @@
-const { db } = require("../connection");
+const db = require("../connection");
 
-async function queries(db) {
-  try {
-    const allUsers = await db.query(`SELECT * FROM users`);
-    console.log(allUsers);
-  } catch (err) {
-    console.log(`Error:\n${err}`);
-  }
+async function queries() {
+  const allUserNames = await db.query(`SELECT username FROM users`);
+  const articlesAboutCoding = await db.query(
+    `SELECT title FROM articles WHERE articles.topic = 'coding'`
+  );
+  const commentsWithZeroVotes = await db.query(
+    `SELECT comments.author, articles.title FROM articles JOIN comments ON articles.article_id = comments.article_id WHERE comments.votes < 0`
+  );
+  const allTopics = await db.query(`SELECT description, slug FROM topics`);
+  const grumpy19Articles = await db.query(
+    `SELECT title FROM articles WHERE author = 'grumpy19'`
+  );
+  const commentsWith10PlusVotes = await db.query(
+    `SELECT comments.author, articles.title FROM articles JOIN comments ON articles.article_id = comments.article_id WHERE comments.votes > 10`
+  );
+  console.log(commentsWith10PlusVotes);
 }
 
-queries(db);
+queries();
