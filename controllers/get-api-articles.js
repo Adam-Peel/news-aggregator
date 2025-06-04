feab56cd9002fa28de211d3073b23853f6e4a3d3
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-04 14:26:46 +0100

    Code solution to delete 'body' from articles - /api/articles

--- controllers/get-api-articles.js
+++ controllers/get-api-articles.js
@@ -1,9 +1,9 @@
 const { fetchAllArticlesDB } = require("../models/fetch-articles-db");
 
 async function getAllArticlesAPI(request, response) {
-  console.log("getArticlesAPI <- controller");
   try {
     const articles = await fetchAllArticlesDB();
+    //console.log(articles);
     response.status(200).send(articles);
   } catch (err) {
     response.status(500).send(err);
--- models/fetch-articles-db.js
+++ models/fetch-articles-db.js
@@ -2,7 +2,21 @@ const db = require("../db/connection");
 
 async function fetchAllArticlesDB(request, response) {
   try {
-    const { rows } = await db.query(`SELECT * FROM articles`);
+    const { rows } = await db.query(
+      `SELECT
+articles.*,
+COUNT(comments.article_id) AS comment_count
+FROM
+articles
+LEFT JOIN
+comments ON articles.article_id = comments.article_id
+GROUP BY
+articles.article_id`
+    );
+    rows.forEach((article) => {
+      delete article["body"];
+    });
+    console.log(rows);
     return { articles: rows };
   } catch (err) {
     return err;
