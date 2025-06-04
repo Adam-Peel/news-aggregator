9d4e9e36129ffcdf3053dd4bb4852cb72b97081f
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-04 11:01:30 +0100

    Create listen and app files

--- listen.js
+++ listen.js
@@ -1,9 +1,11 @@
 const app = require("./app.js");
 
-app.listen(9090, (err) => {
+const listen = app.listen(9090, (err) => {
   if (err) {
     console.log(`Error:\n${err}`);
   } else {
     console.log(`Server listening on port 9090`);
   }
 });
+
+module.exports = { listen };
