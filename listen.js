cc369aff3c152263613301d97e1d35ed6901f607
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-09 12:47:15 +0100

    Alter listen file to test api hosting

--- listen.js
+++ listen.js
@@ -1,12 +1,14 @@
 const app = require("./app.js");
 const { PORT = 9090 } = process.env;
 
-const listen = app.listen(PORT, (err) => {
-  if (err) {
-    console.log(`Error:\n${err}`);
-  } else {
-    console.log(`Server listening on port 9090`);
-  }
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+// const listen = app.listen(PORT, (err) => {
+//   if (err) {
+//     console.log(`Error:\n${err}`);
+//   } else {
+//     console.log(`Server listening on port 9090`);
+//   }
+// });
 
 module.exports = { listen };
